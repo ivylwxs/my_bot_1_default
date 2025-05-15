d6850b0bed14c307bc4ad789052819fcfaca639c
aziza <aziza.abduraufova7@gmail.com>
2025-05-15 15:14:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import asyncio
 import logging
 import sys
-import html
 from os import getenv
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, html, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
@@ -19,8 +18,27 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    safe_name = html.escape(message.from_user.full_name)
-    await message.answer(f"Hello, <b>{safe_name}</b>", parse_mode="HTML")
+    await message.answer(f"Salom, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message(F.text == 'getMe')
+async def get_me(message: Message):
+    chat_id = message.chat.id
+    fullname = message.from_user.full_name
+    text = message.text
+    username = message.from_user.username
+    await message.answer(f'Custom getME! \n\n'
+                         f'chat id: {chat_id}\n'
+                         f'fullname: {fullname}\n'
+                         f'text: {text}\n'
+                         f'username: @{username}')
+
+
+# function - birthday, age,
+
+@dp.message(F.text.regexp(r'[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'))
+async def hadnle_email(message: Message):
+    await message.answer('Siz email yubordingiz!')
 
 
 @dp.message()
@@ -28,7 +46,7 @@ async def echo_handler(message: Message) -> None:
     try:
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        await message.answer("Nice try!")
+        await message.answer("Yana urinib ko'ring!")
 
 
 async def main() -> None:
@@ -40,4 +58,3 @@ async def main() -> None:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-    print('Ok')
